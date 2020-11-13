d56439781598c9f4ffdef5d669e6abfcd657af61
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-11-13 20:45:15 +0200

    fixes

--- HW2-Сonditions-loops/js/main.js
+++ HW2-Сonditions-loops/js/main.js
@@ -1,22 +1,22 @@
 let reloadPage
 
 do{
-    const N = parseInt(prompt("Enter first number"))
-    const M = parseInt(prompt("Enter secound number"))
+    const MIN_VALUE = parseInt(prompt("Enter first number"))
+    const MAX_VALUE = parseInt(prompt("Enter secound number"))
     const SKIP_EVEN = confirm("Пропускати парні числа?")
     let sum = 0
     reloadPage = false
 
-    if(!isNaN(N) && !isNaN(M)){
-        const IS_N_ODD = N & 1
-        let i = (!IS_N_ODD && SKIP_EVEN)? N+1:N //make even number odd if user need to skip even numbers
+    if(!isNaN(MIN_VALUE) && !isNaN(MAX_VALUE)){
+        const IS_N_ODD = MIN_VALUE & 1
+        let i = (!IS_N_ODD && SKIP_EVEN)? MIN_VALUE+1:MIN_VALUE //make even number odd if user need to skip even numbers
 
-        for(i; i <= M; SKIP_EVEN? i+=2: i++){
+        for(i; i <= MAX_VALUE; SKIP_EVEN? i+=2: i++){
             sum+=i
         }
 
-        document.write(`Your first number: ${N} <br>
-        Your secound number: ${M} <br>
+        document.write(`Your first number: ${MIN_VALUE} <br>
+        Your secound number: ${MAX_VALUE} <br>
         You choose ${SKIP_EVEN?"to skip even numbers":"not to skip even numbers"} <br>
         Your sum: ${sum}`)  
     }else{
