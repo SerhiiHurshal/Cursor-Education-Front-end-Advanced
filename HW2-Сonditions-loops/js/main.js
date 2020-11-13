b4094e43b5509322b7afb8485d8e5400cad3a614
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-11-13 20:40:42 +0200

    add support to reload page

--- HW2-Сonditions-loops/js/main.js
+++ HW2-Сonditions-loops/js/main.js
@@ -1,20 +1,26 @@
-const N = parseInt(prompt("Enter first number"))
-const M = parseInt(prompt("Enter secound number"))
-const SKIP_EVEN = confirm("Пропускати парні числа?")
-let sum = 0
+let reloadPage
 
-if(!isNaN(N) && !isNaN(M)){
-    const IS_N_ODD = N & 1
-    let i = (!IS_N_ODD && SKIP_EVEN)? N+1:N //make even number odd if user need to skip even numbers
+do{
+    const N = parseInt(prompt("Enter first number"))
+    const M = parseInt(prompt("Enter secound number"))
+    const SKIP_EVEN = confirm("Пропускати парні числа?")
+    let sum = 0
+    reloadPage = false
 
-    for(i; i <= M; SKIP_EVEN? i+=2: i++){
-        sum+=i
+    if(!isNaN(N) && !isNaN(M)){
+        const IS_N_ODD = N & 1
+        let i = (!IS_N_ODD && SKIP_EVEN)? N+1:N //make even number odd if user need to skip even numbers
+
+        for(i; i <= M; SKIP_EVEN? i+=2: i++){
+            sum+=i
+        }
+
+        document.write(`Your first number: ${N} <br>
+        Your secound number: ${M} <br>
+        You choose ${SKIP_EVEN?"to skip even numbers":"not to skip even numbers"} <br>
+        Your sum: ${sum}`)  
+    }else{
+        reloadPage = confirm("Wrong number! Try again?")
+        console.log(reloadPage)
     }
-
-    document.write(`Your first number: ${N} <br>
-    Your secound number: ${M} <br>
-    You choose ${SKIP_EVEN?"to skip even numbers":"not to skip even numbers"} <br>
-    Your sum: ${sum}`)  
-}else{
-    alert("Wrong number!")
-}
+}while(reloadPage !== false)
