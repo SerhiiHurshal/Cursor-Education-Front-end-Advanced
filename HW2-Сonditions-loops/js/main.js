40459fde9d14887d9fc0d99b3809b76f4f8ecaaf
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-11-22 22:37:20 +0200

    fixes to HW2

--- HW2-Сonditions-loops/js/main.js
+++ HW2-Сonditions-loops/js/main.js
@@ -2,24 +2,28 @@ let reloadPage
 
 do{
     const MIN_VALUE = parseInt(prompt("Enter first number"))
+    if (isNaN(MIN_VALUE)){
+        reloadPage = confirm("Wrong number! Try again?")
+        continue
+    }
     const MAX_VALUE = parseInt(prompt("Enter secound number"))
-    const SKIP_EVEN = confirm("Пропускати парні числа?")
+    if (isNaN(MAX_VALUE) || MIN_VALUE > MAX_VALUE){
+        reloadPage = confirm("Wrong number! Try again?")
+        continue
+    }
+    const SKIP_EVEN = confirm("Skip even numbers?")
     let sum = 0
     reloadPage = false
 
-    if(!isNaN(MIN_VALUE) && !isNaN(MAX_VALUE)){
-        const IS_N_ODD = MIN_VALUE & 1
-        let i = (!IS_N_ODD && SKIP_EVEN)? MIN_VALUE+1:MIN_VALUE //make even number odd if user need to skip even numbers
+    const IS_N_ODD = MIN_VALUE & 1
+    let i = (!IS_N_ODD && SKIP_EVEN)? MIN_VALUE+1:MIN_VALUE //make even number odd if user need to skip even numbers
 
-        for(i; i <= MAX_VALUE; SKIP_EVEN? i+=2: i++){
-            sum+=i
-        }
-
-        document.write(`Your first number: ${MIN_VALUE} <br>
-        Your secound number: ${MAX_VALUE} <br>
-        You choose ${SKIP_EVEN?"to skip even numbers":"not to skip even numbers"} <br>
-        Your sum: ${sum}`)  
-    }else{
-        reloadPage = confirm("Wrong number! Try again?")
+    for(i; i <= MAX_VALUE; SKIP_EVEN? i+=2: i++){
+        sum+=i
     }
+
+    document.write(`Your first number: ${MIN_VALUE} <br>
+    Your secound number: ${MAX_VALUE} <br>
+    You choose ${SKIP_EVEN?"to skip even numbers":"not to skip even numbers"} <br>
+    Your sum: ${sum}`)  
 }while(reloadPage !== false)
