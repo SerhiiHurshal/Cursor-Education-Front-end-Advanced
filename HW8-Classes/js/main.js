6eb76f25f0a5636c8548e2fa917a33dc23650bdb
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-12-08 00:52:47 +0200

    HW-8

---
+++ HW8-Classes/js/main.js
@@ -0,0 +1,57 @@
+class Student{
+    constructor(university, course, fullName){
+        this.university = university
+        this.course = course
+        this.fullName = fullName
+        this.marks = []
+        this.isDismissed = false
+    }
+
+    getInfo(){
+        return `Студент ${this.course} курсу ${this.university}, ${this.fullName}`
+    }
+
+    getAverageMark(){
+        if(!this.marks.length || this.isDismissed) return null
+
+        const sumOfNumbers = this.marks.reduce((total, number) => +total + +number)
+        return (sumOfNumbers/this.marks.length).toFixed(1)
+    }
+
+    get getMarks(){
+        return (this.isDismissed)? null: this.marks
+    }
+
+    set setMark(mark){
+        if(this.isDismissed){
+            return null
+        } else{
+            this.marks.push(mark)
+        }
+    }
+
+    get dismissed(){
+        return this.isDismissed
+    }
+
+    dismiss(){
+        this.isDismissed = true
+    }
+
+    recover(){
+        this.isDismissed = false
+    }
+
+}
+
+
+class BudgetStudent extends Student{
+    constructor(university, course, fullName, isDismissed){
+        super(university, course, fullName, isDismissed)
+        const interval = setInterval(() => {
+            if(this.getAverageMark() > 4 && !this.isDismissed){
+                console.log(`Студент, ${this.fullName}, отримав 1400 грн. стипендії`)
+            }
+        },30000)
+    }
+} 
