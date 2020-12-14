28b5f36f8c0c6e54441c0105fb1cb8601085fcf7
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-12-14 22:27:00 +0200

    fixes to HW10

--- HW10-Events/js/main.js
+++ HW10-Events/js/main.js
@@ -1,30 +1,40 @@
+counter_of_pressing_keys = 0
+
 document.onkeydown = function(e){
     const audio = new Audio()
 
     switch(e.code){
         case "Digit1":
             audio.src = "sounds/piano-e_E_major.wav"
+            counter_of_pressing_keys += 1
             break
         case "Digit2":
             audio.src = "sounds/piano-a_A_major.wav"
+            counter_of_pressing_keys += 1
             break
         case "Digit3":
             audio.src = "sounds/piano-d_D_major.wav"
+            counter_of_pressing_keys += 1
             break   
         case "Digit4":
             audio.src = "sounds/piano-g_G_major.wav"
+            counter_of_pressing_keys += 1
             break 
         case "Digit5":
             audio.src = "sounds/piano-c_C_major.wav"
+            counter_of_pressing_keys += 1
             break
         case "Digit6":
             audio.src = "sounds/piano-f_F_major.wav"
+            counter_of_pressing_keys += 1
             break
         case "Digit7":
             audio.src = "sounds/piano-b_B_major.wav"
+            counter_of_pressing_keys += 1
             break
     }
 
+    audio.volume = 0.01
     audio.play()
 }
 
@@ -33,14 +43,23 @@ const btn_Merry_Christmas = document.getElementById("Merry Christmas")
 const btn_Carol_of_the_bells = document.getElementById("Carol of the bells")
 
 btn_Jingle_Bells.onclick = function(){
-    alert("You win!")
-    window.location.href = "https://www.youtube.com/watch?v=IqrMDbKXD-4"
+    if(counter_of_pressing_keys > 20){
+        alert("You win!")
+        window.location.href = "https://www.youtube.com/watch?v=IqrMDbKXD-4"
+    }else{
+        alert("Do not cheat. Santa won't bring you presents!!!")
+    }
 }
 
 const playLoseSound = () =>{
-    const audio = new Audio()
-    audio.src = "sounds/zapsplat_multimedia_game_error_tone_001_24919.mp3"
-    audio.play()
+    if(counter_of_pressing_keys > 20){
+        const audio = new Audio()
+        audio.src = "sounds/zapsplat_multimedia_game_error_tone_001_24919.mp3"
+        audio.volume = 0.2
+        audio.play()
+    }else{
+            alert("Do not cheat. Santa won't bring you presents!!!")
+        }
 }
 
 btn_Merry_Christmas.onclick = function(){playLoseSound()}
