eed7b22153525a3db5b1f83b6ea94d56f23b465d
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-12-14 20:24:48 +0200

    HW10

---
+++ HW10-Events/js/main.js
@@ -0,0 +1,48 @@
+document.onkeydown = function(e){
+    const audio = new Audio()
+
+    switch(e.code){
+        case "Digit1":
+            audio.src = "sounds/piano-e_E_major.wav"
+            break
+        case "Digit2":
+            audio.src = "sounds/piano-a_A_major.wav"
+            break
+        case "Digit3":
+            audio.src = "sounds/piano-d_D_major.wav"
+            break   
+        case "Digit4":
+            audio.src = "sounds/piano-g_G_major.wav"
+            break 
+        case "Digit5":
+            audio.src = "sounds/piano-c_C_major.wav"
+            break
+        case "Digit6":
+            audio.src = "sounds/piano-f_F_major.wav"
+            break
+        case "Digit7":
+            audio.src = "sounds/piano-b_B_major.wav"
+            break
+    }
+
+    audio.play()
+}
+
+const btn_Jingle_Bells = document.getElementById("Jingle Bells")
+const btn_Merry_Christmas = document.getElementById("Merry Christmas")
+const btn_Carol_of_the_bells = document.getElementById("Carol of the bells")
+
+btn_Jingle_Bells.onclick = function(){
+    alert("You win!")
+    window.location.href = "https://www.youtube.com/watch?v=IqrMDbKXD-4"
+}
+
+const playLoseSound = () =>{
+    const audio = new Audio()
+    audio.src = "sounds/zapsplat_multimedia_game_error_tone_001_24919.mp3"
+    audio.play()
+}
+
+btn_Merry_Christmas.onclick = function(){playLoseSound()}
+btn_Carol_of_the_bells.onclick = function(){playLoseSound()}
+
