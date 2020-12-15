ec003954ffb8bfe29adb37309192fc2760bd5fe9
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-12-15 23:55:04 +0200

    fixes to HW10

--- HW10-Events/js/main.js
+++ HW10-Events/js/main.js
@@ -1,4 +1,4 @@
-counter_of_pressing_keys = 0
+counterOfPressingKeys = 0
 
 document.onkeydown = function(e){
     const audio = new Audio()
@@ -6,31 +6,31 @@ document.onkeydown = function(e){
     switch(e.code){
         case "Digit1":
             audio.src = "sounds/piano-e_E_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break
         case "Digit2":
             audio.src = "sounds/piano-a_A_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break
         case "Digit3":
             audio.src = "sounds/piano-d_D_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break   
         case "Digit4":
             audio.src = "sounds/piano-g_G_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break 
         case "Digit5":
             audio.src = "sounds/piano-c_C_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break
         case "Digit6":
             audio.src = "sounds/piano-f_F_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break
         case "Digit7":
             audio.src = "sounds/piano-b_B_major.wav"
-            counter_of_pressing_keys += 1
+            counterOfPressingKeys += 1
             break
     }
 
@@ -38,12 +38,12 @@ document.onkeydown = function(e){
     audio.play()
 }
 
-const btn_Jingle_Bells = document.getElementById("Jingle Bells")
-const btn_Merry_Christmas = document.getElementById("Merry Christmas")
-const btn_Carol_of_the_bells = document.getElementById("Carol of the bells")
+const btnJingleBells = document.getElementById("Jingle Bells")
+const btnMerryChristmas = document.getElementById("Merry Christmas")
+const btnCarolOfTheBells = document.getElementById("Carol of the bells")
 
-btn_Jingle_Bells.onclick = function(){
-    if(counter_of_pressing_keys > 20){
+btnJingleBells.onclick = function(){
+    if(counterOfPressingKeys > 20){
         alert("You win!")
         window.location.href = "https://www.youtube.com/watch?v=IqrMDbKXD-4"
     }else{
@@ -52,7 +52,7 @@ btn_Jingle_Bells.onclick = function(){
 }
 
 const playLoseSound = () =>{
-    if(counter_of_pressing_keys > 20){
+    if(counterOfPressingKeys > 20){
         const audio = new Audio()
         audio.src = "sounds/zapsplat_multimedia_game_error_tone_001_24919.mp3"
         audio.volume = 0.2
@@ -62,6 +62,6 @@ const playLoseSound = () =>{
         }
 }
 
-btn_Merry_Christmas.onclick = function(){playLoseSound()}
-btn_Carol_of_the_bells.onclick = function(){playLoseSound()}
+btnMerryChristmas.onclick = function(){playLoseSound()}
+btnCarolOfTheBells.onclick = function(){playLoseSound()}
 
