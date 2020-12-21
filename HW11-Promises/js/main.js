aab7910cd181bb85f8dc1670583f0151e9283f66
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-12-21 19:04:50 +0200

    HW11

---
+++ HW11-Promises/js/main.js
@@ -0,0 +1,20 @@
+async function getRandomChinese(length) {
+    const start = Date.now()
+
+    let result = ""
+
+    const getSing = () => new Promise((resolve) => setTimeout(() => {
+        return resolve(String.fromCharCode(Date.now().toString().slice(-5)))
+    }, 50))
+
+    while (length > 0) {
+        result += await getSing()
+        length--
+    }
+
+    console.log(Date.now() - start)
+
+    return result
+}
+
+getRandomChinese(6).then((result) => document.writeln(result))
