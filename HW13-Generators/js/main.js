c78ab84fa8003b2f296ce80e6bedf1249a20390c
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2020-12-27 18:30:11 +0200

    HW13 main commit

--- HW13-Generators/js/main.js
+++ HW13-Generators/js/main.js
@@ -14,3 +14,38 @@ const getID = () => {
 
 document.getElementById("genIdBtn").addEventListener("click", getID)
 
+
+function* fontSize(n = 14) {
+    let fontSize = n
+
+    while(true){
+        const commond = yield fontSize
+        if(commond === '+') {
+            yield fontSize = fontSize + 2
+        } else {
+            yield fontSize = fontSize - 2
+        }
+    }
+}
+
+
+const changeFontSize = fontSize()
+
+
+const fontSizeUp = () => {
+    console.log(changeFontSize.next('+').value)
+
+    const newFontSize = changeFontSize.next('+').value
+    document.getElementById("idHeader").style.fontSize = `${newFontSize}px`
+}
+
+const fontSizeDown = () => {
+    console.log(changeFontSize.next('-').value)
+
+    const newFontSize = changeFontSize.next('-').value
+    document.getElementById("idHeader").style.fontSize = `${newFontSize}px`
+}
+
+
+document.getElementById("sizeUpBtn").addEventListener("click", fontSizeUp)
+document.getElementById("sizeDownBtn").addEventListener("click", fontSizeDown)
