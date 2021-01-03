6c87d5fd503eba538d87ef8e47d8528ba05f0f2e
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2021-01-03 13:29:43 +0200

    make push methods immutable

--- HW4-Arrays/js/main.js
+++ HW4-Arrays/js/main.js
@@ -1,88 +1,83 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"]
-const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"]
+const students = ['Олександр', 'Ігор', 'Олена', 'Іра', 'Олексій', 'Світлана']
+const themes = ['Диференційне рівняння', 'Теорія автоматів', 'Алгоритми і структури даних']
 const marks = [4, 5, 5, 3, 4, 5]
 
-
 const makePairs = (students) => {
-    const studentsWithPair = []
-    const pairs = []
-    const pair = []
+  let studentsWithPair = []
+  let pairs = []
+  let pair = []
 
-    for (student of students){
-        //finding girl without pair
-        if (student.charAt(student.length-1) == 'а' && !studentsWithPair.includes(student)){
-            pair.push(student)
-            studentsWithPair.push(student)
+  for (student of students) {
+    //finding girl without pair
+    if (student.charAt(student.length - 1) == 'а' && !studentsWithPair.includes(student)) {
+      pair = [...pair, student]
+      studentsWithPair = [...studentsWithPair, student]
 
-            for(student of students){
-                //finding boy without pair
-                if (student.charAt(student.length-1) !== 'а' && !studentsWithPair.includes(student)){
-                    pair.push(student)
-                    studentsWithPair.push(student)
-                    break
-                }
-            }
-
-            pairs.push(pair.slice(0)) //copy pair to array of pairs
-            pair.splice(0) //clean pair
+      for (student of students) {
+        //finding boy without pair
+        if (student.charAt(student.length - 1) !== 'а' && !studentsWithPair.includes(student)) {
+          pair = [...pair, student]
+          studentsWithPair = [...studentsWithPair, student]
+          break
         }
-    }
+      }
 
-    return pairs
+      pairs = [...pairs, pair.slice(0)] //copy pair to array of pairs
+      pair.splice(0) //clean pair
+    }
+  }
+
+  return pairs
 }
 
-
 const teamToProject = (teams, projects) => {
-    const teamToProjectArr = []
-    let pair = ''
+  let teamToProjectArr = []
+  let pair = ''
 
-    for (i = 0; i < teams.length; i++){
-        pair += teams[i][0] + " і " + teams[i][1]
-        teamToProjectArr.push([pair, projects[i]])
-        pair = ''
-    }
+  for (i = 0; i < teams.length; i++) {
+    pair += teams[i][0] + ' і ' + teams[i][1]
+    teamToProjectArr = [...teamToProjectArr, [pair, projects[i]]]
+    pair = ''
+  }
 
-    return teamToProjectArr
+  return teamToProjectArr
 }
 
 const studentsGrades = (students, grades) => {
-    const studentsGradesArr = []
+  let studentsGradesArr = []
 
-    for (i = 0; i < students.length; i++){
-        studentsGradesArr.push([students[i], grades[i]])
-    }
+  for (i = 0; i < students.length; i++) {
+    studentsGradesArr = [...studentsGradesArr, [students[i], grades[i]]]
+  }
 
-    return studentsGradesArr
+  return studentsGradesArr
 }
-
 
 const teamsGrades = (teams) => {
-    const teamToGrade = []
-    let pair = ''
-    let grade
+  let teamToGrade = []
+  let pair = ''
+  let grade
 
-    for (team of teams){
-        pair += team[0] + " і " + team[1]
-        grade = Math.floor(Math.random() * (6 - 1) + 1)
-        teamToGrade.push([pair, grade])
-        pair = ''
-    }
+  for (team of teams) {
+    pair += team[0] + ' і ' + team[1]
+    grade = Math.floor(Math.random() * (6 - 1) + 1)
+    teamToGrade = [...teamToGrade, [pair, grade]]
+    pair = ''
+  }
 
-    return teamToGrade
+  return teamToGrade
 }
-
 
 const showArray = (array) => {
-    let prettyArray = "["
-    for (element of array){
-        prettyArray += `[${element}]`
-    }
-    
-    prettyArray += "]"
+  let prettyArray = '['
+  for (element of array) {
+    prettyArray += `[${element}]`
+  }
 
-    return prettyArray
+  prettyArray += ']'
+
+  return prettyArray
 }
-
 
 document.writeln(`Пари студентів: ${showArray(makePairs(students))} <br>`)
 
