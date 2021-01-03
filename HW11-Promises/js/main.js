f341f62017266c0de028677f6bb70f0ec96c7f35
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2021-01-03 13:50:28 +0200

    add info about console log

--- HW11-Promises/js/main.js
+++ HW11-Promises/js/main.js
@@ -1,20 +1,23 @@
 async function getRandomChinese(length) {
-    const start = Date.now()
+  const start = Date.now()
 
-    let result = ""
+  let result = ''
 
-    const getSing = () => new Promise((resolve) => setTimeout(() => {
+  const getSing = () =>
+    new Promise((resolve) =>
+      setTimeout(() => {
         return resolve(String.fromCharCode(Date.now().toString().slice(-5)))
-    }, 50))
+      }, 50)
+    )
 
-    while (length > 0) {
-        result += await getSing()
-        length--
-    }
+  while (length > 0) {
+    result += await getSing()
+    length--
+  }
 
-    console.log(Date.now() - start)
+  console.log(`Time spent on this operation: ${Date.now() - start} milliseconds`)
 
-    return result
+  return result
 }
 
 getRandomChinese(6).then((result) => document.writeln(result))
